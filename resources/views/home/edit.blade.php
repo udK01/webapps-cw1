008820f372e31e7c5345c9bab5e8e64bcf90134c
udK01 <dominik.muranyi@gmail.com>
2023-01-02 07:38:04 +0000

    Fixed buttons on edit view

--- resources/views/home/edit.blade.php
+++ resources/views/home/edit.blade.php
@@ -11,7 +11,7 @@
         <div class="postBox">
             <div class="box">
                 {{-- User Information --}}
-                <div style="display: flex;justify-content: space-evenly;">
+                <div style="display: flex;justify-content: space-evenly;margin: 10px">
                     <a href="{{ route('home.profile', ['id' => $post->user->id]) }}">
                         <div class="username" style="display:flex;flex-direction: column;align-items: center;">
                             {{$post->user->name}}
@@ -46,7 +46,7 @@
                     </div>
                 @endif
                 {{-- Icons --}}
-                <div style="display:flex;items-align:space-evenly;justify-content:space-evenly;">
+                <div style="display:flex;items-align:space-evenly;justify-content:space-evenly;margin:10px">
                     {{-- Like Icon --}}
                     <div style="display: flex;"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" d="M6.633 10.5c.806 0 1.533-.446 2.031-1.08a9.041 9.041 0 012.861-2.4c.723-.384 1.35-.956 1.653-1.715a4.498 4.498 0 00.322-1.672V3a.75.75 0 01.75-.75A2.25 2.25 0 0116.5 4.5c0 1.152-.26 2.243-.723 3.218-.266.558.107 1.282.725 1.282h3.126c1.026 0 1.945.694 2.054 1.715.045.422.068.85.068 1.285a11.95 11.95 0 01-2.649 7.521c-.388.482-.987.729-1.605.729H13.48c-.483 0-.964-.078-1.423-.23l-3.114-1.04a4.501 4.501 0 00-1.423-.23H5.904M14.25 9h2.25M5.904 18.75c.083.205.173.405.27.602.197.4-.078.898-.523.898h-.908c-.889 0-1.713-.518-1.972-1.368a12 12 0 01-.521-3.507c0-1.553.295-3.036.831-4.398C3.387 10.203 4.167 9.75 5 9.75h1.053c.472 0 .745.556.5.96a8.958 8.958 0 00-1.302 4.665c0 1.194.232 2.333.654 3.375z" /></svg>                      
                     {{$post->likes}}</div>
@@ -64,17 +64,17 @@
     <div class="box">
         <div class="buttons">
             {{-- Back --}}
-            <a href="{{ route('home.show', ['id' => $post->id]) }}" class="btn btn-2" style="color: #F0F4EF">Back</a>
+            <a href="{{ route('home.show', ['id' => $post->id]) }}" class="btn btn-2">Back</a>
                 
             {{-- Delete --}}
-            <form method="POST" action="{{ route('home.destroy', ['id' => $post->id]) }}" class="btn btn-2" style="color: #F0F4EF;">
+            <form method="POST" action="{{ route('home.destroy', ['id' => $post->id]) }}" class="btn btn-2" style="top:8px;">
                 @csrf
                 @method('DELETE')
                 <a onclick='return confirm("Are you sure?")'><button type="submit">Delete</button></a>
             </form>
             
             {{-- Submit --}}
-            <button type="submit" form="postForm" class="btn btn-2" style="color: #F0F4EF;">Submit</button>
+            <button type="submit" form="postForm" class="btn btn-2">Submit</button>
         </div>
     </div>     
 @endif
@@ -111,6 +111,7 @@
     .buttons {
         display: flex;
         justify-content: space-evenly;
+        align-items: center;
         margin: 10px;
     }
 
@@ -122,7 +123,7 @@
     }
 
     .btn-2 {
-        color: black;
+        color: #F0F4EF;
     }
 
     .btn-2::after, .btn-2::before {
